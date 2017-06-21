0e331d66ce9aca4acf525b013e706c1074ec1ced
Julius Kiano <jgkiano@gmail.com>
2017-06-21 09:35:49 +0300

    imporved ui, added continue button, created feed file

--- screens/Interests.js
+++ screens/Interests.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, Dimensions, ScrollView, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Dimensions, ScrollView, Image, LayoutAnimation } from 'react-native';
 import { Item, Input, Label, Button, Picker } from 'native-base';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -85,12 +85,52 @@ const DATA = {
 };
 
 class Interests extends Component {
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
+
+    state = {
+        progress: 0,
+        interests: []
+    };
+
+    renderBorder(id) {
+        if(this.state.interests.indexOf(id) > -1) {
+            return {
+                borderWidth: 4,
+                borderColor: '#FF6F00'
+            };
+        }
+        return;
+    }
+
+    renderProgress = (id) => {
+        if(this.state.interests.indexOf(id) === -1) {
+            let interests = this.state.interests;
+            interests.push(id);
+            this.setState({
+                progress: this.state.progress + 0.2,
+                interests
+            });
+            console.log(this.state.interests)
+        } else {
+            let interests = this.state.interests;
+            interests.splice(interests.indexOf(id), 1);
+            this.setState({
+                progress: this.state.progress - 0.2,
+                interests
+            });
+            console.log(this.state.interests)
+        }
+    }
+
+
     renderInterests() {
         return DATA.interests.map((interest) => {
             return (
-                <TouchableOpacity key={interest.id}>
+                <TouchableOpacity onPress={() => {this.renderProgress(interest.id)}} key={interest.id}>
                     <View style={{width: SCREEN_WIDTH / 3, height: SCREEN_WIDTH / 3, position:'relative'}}>
-                        <View style={{position: 'absolute', left: 8, right: 8, bottom: 8, top: 8, backgroundColor: 'white', alignItems: 'center', justifyContent:'center'}}>
+                        <View style={[{position: 'absolute', left: 8, right: 8, bottom: 8, top: 8, backgroundColor: 'white', alignItems: 'center', justifyContent:'center' }, this.renderBorder(interest.id)]}>
                             <Image source={interest.icon} style={{width: 50, height: 50}} />
                             <Text style={{marginTop: 6}}>{interest.name}</Text>
                         </View>
@@ -100,15 +140,27 @@ class Interests extends Component {
         });
     }
 
+    renderContinueButton() {
+        if( this.state.interests.length >= 5) {
+            return(
+                <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                    <Button style={{backgroundColor: '#FF6F00', alignItems: 'center'}}><Text style={{color: 'white', fontWeight: 'bold'}}>You're all set! Start Your Adventure</Text></Button>
+                </View>
+            );
+        }
+        return;
+    }
+
     render() {
         return (
             <View style={{flex: 1}}>
-                <View style={{flex: 1, justifyContent: 'center'}}>
-                    <Text style={{textAlign: 'center', fontSize: 24, fontWeight: 'bold', color: '#FF6F00'}}>Let's Get Personal</Text>
-                    <Text style={{textAlign: 'center', fontSize: 18, color: '#333', padding: 16}}>For a personalized experience we need to know what are you in to. Choose your favorite interests</Text>
+                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                    <Text style={{textAlign: 'center', fontSize: 22, fontWeight: 'bold', color: '#FF6F00'}}>Let's Get Personal</Text>
+                    <Text style={{textAlign: 'center', fontSize: 16, color: '#333', padding: 16}}>For a personalized experience we need to know what are you in to. Choose your favorite interests</Text>
+                    {this.renderContinueButton()}
                 </View>
                 <View style={{height: 6, width: SCREEN_WIDTH, backgroundColor:'#F5F5F5'}}>
-                    <View style={{height: 6, width: SCREEN_WIDTH / 3, backgroundColor: '#FF6F00'}} />
+                    <View style={{height: 6, width: SCREEN_WIDTH * this.state.progress, backgroundColor: '#FF6F00'}} />
                 </View>
                 <View style={{flex: 2, backgroundColor: '#EEEEEE', flexDirection:'row', flexWrap: 'wrap'}}>
                     <ScrollView contentContainerStyle={{flexDirection: 'row', flexWrap: 'wrap'}}>
